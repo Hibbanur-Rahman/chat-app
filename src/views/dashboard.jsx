39a1ac52f18281c8d18bac81c827d0dbfbc57b54
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-05-18 19:13:22 +0530

    adding the ui of register and login pages

---
+++ src/views/dashboard.jsx
@@ -0,0 +1,9 @@
+const DashBoard=()=>{
+    return (
+        <>
+            <h1> chat dashboard</h1>
+        </>
+    )
+}
+
+export default DashBoard;
---
+++ src/views/login.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo-light.png";
+import loginImg from '../assets/images/login-img.png';
+const Login = () => {
+  return (
+    <div className="login row m-0 p-0 pt-5 ps-3 pe-3">
+      <div className="col-lg-4 d-lg-flex d-none position-relative ">
+        <div className="logo">
+
+        <img src={logo} alt="logo" className="img-fluid" />
+        </div>
+        <div className="login-img">
+            <img src={loginImg} alt="" className="position-absolute" />
+        </div>
+      </div>
+      <div className="col-lg-8 col-12 d-flex flex-column  bg-light rounded-4 p-3  align-items-center">
+        <h1 className="m-0 p-0 fw-medium mt-3 text-center">Welcome Back !</h1>
+        <p className=" fs-6 m-0 p-0 fw-medium mt-2 text-center text-secondary ">
+          Sign in to continue to WebChat
+        </p>
+        <form action="" className="col-8 mt-5">
+          <div class="mb-3">
+            <label for="email" class="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              placeholder="hibbanrahmanhyt@gmail.com"
+            />
+          </div>
+          <div class="mb-3">
+            <div className="row m-0 p-0 justify-content-between ">
+              <label for="password" class="form-label w-auto ms-0 p-0">
+                Password
+              </label>
+              <Link
+                to="/forgot"
+                className="m-0 p-0 w-auto text-secondary text-decoration-none"
+              >
+                <p className="m-0 p-0 w-auto">Forgot password?</p>
+              </Link>
+            </div>
+
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              placeholder="*******"
+            />
+          </div>
+          <div className="row m-0 p-0 mt-5">
+            <button className="btn btn-ctm text-light">Log In</button>
+          </div>
+          <div className="row m-0 p-0 justify-content-center align-items-center mt-5">
+
+          <p className="m-0 p-0 text-secondary text-center w-auto">Don't have an account ? </p>
+          <Link to='/register' className="text-decoration-none w-auto m-0 p-0"><p className="m-0 p-0 text-decoration-underline w-auto" style={{color:"#4EAC6D"}}> Register</p></Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
