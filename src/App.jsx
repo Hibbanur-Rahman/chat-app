05f45b809c6cc3c1556799ca8b26176b5218b2c8
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-05-18 19:16:37 +0530

    adding the styles and routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import './assets/styles/main.scss';
-function App() {
+import { Routes, Route } from "react-router-dom";
 
+import "./assets/styles/main.scss";
+import Login from "./views/login";
+import Register from "./views/register";
+import DashBoard from "./views/dashboard";
+function App() {
   return (
-    <>
-      <h1>hello chat app</h1>     
-    </>
-  )
+    <div className="row m-0 p-0">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path='/register' element={<Register/>}/>
+        <Route path="/dashboard" element={<DashBoard/>}/>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
